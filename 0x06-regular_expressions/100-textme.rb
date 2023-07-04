050dfc30e98d9d0973579520af770b27d50172a7
clncheruiyot <clncheruiyot@gmail.com>
2023-07-04 04:13:32 -0700

    100-textme.rb

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,3 +1,3 @@
 #!/usr/bin/env ruby
 # A regular expression matches the given pattern
-puts ARGV[0].scan(/\[Start at:(.*?)\]\s\[to:(.*?)\]\s\[flags:(.*?)\]/).join(',')
+puts ARGV[0].scan(/\[start at:(.*?)\]\s\[to:(.*?)\]\s\[flags:(.*?)\]/).join(',')
